b0b582f4cb1aef4c542a35cdeddd52038898602b
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2017-05-09 16:59:03 +0200

    Add abstract ListView

--- dist/js/biont-backbone.js
+++ dist/js/biont-backbone.js
@@ -1,17 +1,22 @@
 "use strict";
 
 exports.__esModule = true;
-exports.templates = exports.BiontView = undefined;
+exports.templates = exports.ListView = exports.BiontView = undefined;
 
 var _BiontView = require("./Views/BiontView");
 
 var _BiontView2 = _interopRequireDefault(_BiontView);
+
+var _ListView = require("./Views/ListView");
+
+var _ListView2 = _interopRequireDefault(_ListView);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var templates = [];
 
 exports.BiontView = _BiontView2.default;
+exports.ListView = _ListView2.default;
 exports.templates = templates;
-exports.default = { BiontView: _BiontView2.default, templates: templates };
+exports.default = { BiontView: _BiontView2.default, ListView: _ListView2.default, templates: templates };
 //# sourceMappingURL=biont-backbone.js.map
--- src/js/biont-backbone.js
+++ src/js/biont-backbone.js
@@ -1,8 +1,11 @@
 import BiontView from "./Views/BiontView";
+import ListView from "./Views/ListView";
+
 
 const templates = [];
 
 export {BiontView};
+export {ListView};
 export {templates};
 
-export default {BiontView, templates}
+export default { BiontView, ListView, templates }
