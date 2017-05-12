fb79c895bb573013b36b67a08ef775c1b3ba2882
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2017-05-12 17:56:39 +0200

    Add missing files

---
+++ dist/js/Views/LazyLoadView.js
@@ -0,0 +1,69 @@
+'use strict';
+
+exports.__esModule = true;
+
+var _ScrollSpyView = require('./ScrollSpyView');
+
+var _ScrollSpyView2 = _interopRequireDefault(_ScrollSpyView);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var LazyLoadView = function (_ScrollSpyView$extend) {
+	_inherits(LazyLoadView, _ScrollSpyView$extend);
+
+	function LazyLoadView() {
+		_classCallCheck(this, LazyLoadView);
+
+		return _possibleConstructorReturn(this, _ScrollSpyView$extend.apply(this, arguments));
+	}
+
+	LazyLoadView.prototype.initialize = function initialize() {
+		console.log('lazyloadviewview ' + this.constructor.name);
+
+		_ScrollSpyView$extend.prototype.initialize.call(this);
+		this.queue = {};
+		if (this.constructor.name === 'LazyLoadView') {
+			throw new TypeError("Cannot construct LazyLoadView instances directly");
+		}
+	};
+
+	LazyLoadView.prototype.render = function render(force) {
+		var _this2 = this;
+
+		var rendered = this.rendered;
+		_ScrollSpyView$extend.prototype.render.call(this, force);
+
+		if (!rendered || force) {
+			this.on('scrollEnter', function (data) {
+				if (_this2.queue[data.subview]) {
+					console.log('lazy rendering', _this2.queue[data.subview]);
+					_this2.renderSubView(data.subview, _this2.queue[data.subview].view, _this2.queue[data.subview].element, _this2.queue[data.subview].forced);
+					delete _this2.queue[data.subview];
+				}
+			});
+
+			this.on('scrollLeave', function (data) {
+				// console.log( 'scrollLeave', data );
+			});
+		}
+	};
+
+	LazyLoadView.prototype.shouldRenderSubView = function shouldRenderSubView(handle, view, element, forced) {
+		var inView = this.isInView(element[0]);
+		if (!inView) {
+			this.queue[handle] = { view: view, element: element, forced: forced };
+		}
+		return inView;
+	};
+
+	return LazyLoadView;
+}(_ScrollSpyView2.default.extend());
+
+exports.default = LazyLoadView;
+//# sourceMappingURL=LazyLoadView.js.map
---
+++ dist/tpl/ListView.js
@@ -0,0 +1,27 @@
+function anonymous(locals, escapeFn, include, rethrow
+/**/) {
+escapeFn = escapeFn || function (markup) {
+  return markup == undefined
+    ? ''
+    : String(markup)
+        .replace(_MATCH_HTML, encode_char);
+};
+var _ENCODE_HTML_RULES = {
+      "&": "&amp;"
+    , "<": "&lt;"
+    , ">": "&gt;"
+    , '"': "&#34;"
+    , "'": "&#39;"
+    }
+  , _MATCH_HTML = /[&<>'"]/g;
+function encode_char(c) {
+  return _ENCODE_HTML_RULES[c] || c;
+};
+;
+  var __output = [], __append = __output.push.bind(__output);
+  with (locals || {}) {
+    ; __append("<div class=\"\">\n    <ul class=\"collapsible popout\" data-collapsible=\"expandable\"></ul>\n</div>")
+  }
+  return __output.join("");
+
+}
